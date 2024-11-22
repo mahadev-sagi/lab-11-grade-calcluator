0a8793d960e6d3c927f3c753cf4975cedf079a5a
Mahadev Sagi <madhsa1972@gmail.com>
2024-11-22 16:55:51 -0500

    added all lab files created 2 functions created all functions

--- Lab11.py
+++ Lab11.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import os
+
 
 def menu():
     print('1. Student grade')  # should ask for a student name and calculate their entire grade.
@@ -25,6 +27,24 @@ def find_assignments():
             assignments[assignment_id] = {'name':name,'points':points}
         return assignments
 
+def find_submissions():
+    submissions = []
+    sub_path = 'data/submissions'
+    for i in os.listdir(sub_path):
+        file_path = os.path.join(sub_path,i)
+        with open(file_path,'r') as file:
+            for line in file:
+                student_id,assignment_id,percentage = line.strip().split('|')
+                submissions.append({
+                    'student_id':student_id,
+                    'assignment_id':assignment_id,
+                    'percentage': int(percentage)
+                    })
+    return submissions
+
+
+
+
 
 
 
@@ -44,6 +64,8 @@ def main():
         elif choice == '2':
             assignment_name = input("What is the assignment name:")
             print(find_assignments())
+        elif choice == '3':
+            print(find_submissions())
 
 
 
