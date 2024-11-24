b68d72852f0e5c87beb2ef719c0dda55ea5c1ea4
Mahadev Sagi <madhsa1972@gmail.com>
2024-11-23 22:44:11 -0500

    added all lab files created 2 functions created all functions finished?

--- Lab11.py
+++ Lab11.py
@@ -44,28 +44,68 @@ def find_submissions():
 
 
 
-
-
-
-
-
-
-
-
-
-
 def main():
     while True:
         menu()
         choice = input('\nEnter your selection:')
         if choice == '1':
+            students = find_students()
             student_name = input("What is the student's name:")
-            print(find_students())
+            for student_id, name in students.items():
+                if name == student_name:
+                    total_points = 0
+                    points_earned = 0
+                    submissions = find_submissions()
+                    assignments = find_assignments()
+                    for submission in submissions:
+                        if submission['student_id'] == student_id:
+                            assignment = assignments.get(submission['assignment_id'])
+                            if assignment:
+                                total_points  += assignment['points']
+                                points_earned += (assignment['points']*submission['percentage'])/100
+                    grade = ((points_earned /total_points) *100)
+                    print(f'{grade:.0f}%')
+                    break
+
         elif choice == '2':
             assignment_name = input("What is the assignment name:")
-            print(find_assignments())
+            assignments = find_assignments()
+            submissions = find_submissions()
+            for id, assignment in assignments.items():
+                if assignment['name'] == assignment_name:
+                    scores = []
+                    for submission in submissions:
+                        if submission['assignment_id'] == id:
+                            scores.append(submission['percentage'])
+                    if scores:
+                        print(f'Min: {min(scores)}')
+                        print(f'Avg: {sum(scores)/len(scores):.0f}')
+                        print(f'Max: {max(scores)}')
+
         elif choice == '3':
-            print(find_submissions())
+            assignments = find_assignments()
+            submissions = find_submissions()
+            assignment_name = input('What is the assignment name:')
+            for id, assignment in assignments.items():
+                if assignment['name'] == assignment_name:
+                    scores = []
+                    for submission in submissions:
+                        if submission['assignment_id'] == id:
+                            scores.append(submission['percentage'])
+                    plt.hist(
+                        scores,
+                        bins = [0,25,50,75,100],
+                        edgecolor = 'black'
+                    )
+                    plt.title(f'Distribution for {assignment_name}')
+                    plt.xlabel("Percentage")
+                    plt.ylabel('Number of Students')
+                    plt.show()
+
+
+
+
+
 
 
 
